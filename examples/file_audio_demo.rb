5fdaf1e48ce0752054c3c04667c41113013eb23a
ydah <t.yudai92@gmail.com>
2026-02-22 11:36:14 +0900

    feat: make file audio demo more kinetic

--- examples/file_audio_demo.rb
+++ examples/file_audio_demo.rb
@@ -3,24 +3,31 @@
 Vizcore.define do
   scene :groove do
     layer :background do
-      shader :neon_grid
+      shader :bass_tunnel
       opacity 1.0
+      effect :feedback
+      effect_intensity 0.72
+      rotation_speed 1.4
     end
 
     layer :wireframe do
       type :wireframe_cube
       blend :add
       map fft_spectrum => :deform
-      map frequency_band(:high) => :color_shift
-      map amplitude => :rotation_speed
+      map frequency_band(:mid) => :color_shift
+      map frequency_band(:low) => :rotation_speed
     end
 
     layer :particles do
       type :particle_field
-      count 3200
+      count 4200
       blend :add
+      size 3.8
+      effect :chromatic
+      effect_intensity 0.7
       map amplitude => :speed
       map frequency_band(:low) => :size
+      map frequency_band(:high) => :effect_intensity
     end
 
     layer :title do
@@ -28,7 +35,8 @@ Vizcore.define do
       content "FILE AUDIO DEMO"
       font_size 64
       color "#f2f7ff"
-      glow_strength 0.22
+      glow_strength 0.18
+      map beat? => :glow_strength
     end
   end
 
@@ -36,6 +44,9 @@ Vizcore.define do
     layer :background do
       shader :kaleidoscope
       opacity 0.95
+      effect :glitch
+      effect_intensity 0.86
+      rotation_speed 2.4
     end
 
     layer :wireframe do
@@ -43,14 +54,16 @@ Vizcore.define do
       blend :add
       map fft_spectrum => :deform
       map frequency_band(:high) => :color_shift
-      map amplitude => :rotation_speed
+      map frequency_band(:mid) => :rotation_speed
     end
+
     layer :title do
       type :text
       content "DROP"
       font_size 92
       color "#eaffff"
       glow_strength 0.32
+      map beat? => :glow_strength
     end
   end
 
