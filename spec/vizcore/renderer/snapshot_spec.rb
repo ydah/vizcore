b81c33b4bf3bda86e0bb3029d79a660af6adc068
ydah <t.yudai92@gmail.com>
2026-05-15 22:25:13 +0900

    Add snapshot golden frame test

--- spec/vizcore/renderer/snapshot_spec.rb
+++ spec/vizcore/renderer/snapshot_spec.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
+require "digest"
 require "tmpdir"
+require "zlib"
 require "vizcore/config"
 require "vizcore/renderer/snapshot"
 
@@ -19,5 +21,40 @@ RSpec.describe Vizcore::Renderer::Snapshot do
       expect(File.binread(out, 8)).to eq(Vizcore::Renderer::PngWriter::SIGNATURE)
       expect(File.size(out)).to be > 128
     end
+  end
+
+  it "matches the golden scanline digest for the basic dummy frame" do
+    config = Vizcore::Config.new(
+      scene_file: Vizcore.root.join("examples", "basic.rb").to_s,
+      audio_source: :dummy
+    )
+    frame_source = Vizcore::Renderer::SceneFrameSource.new(config: config)
+    frame_source.start
+    frame = frame_source.capture
+    png = Vizcore::Renderer::SnapshotRenderer.new(width: 160, height: 90).render(
+      scene: frame.fetch(:scene),
+      audio: frame.fetch(:audio)
+    )
+
+    expect(Digest::SHA256.hexdigest(png_scanlines(png))).to eq(
+      "9a90aa759e28ff2f22e05c0bc78e16a9d233adbb6d61cc2f22f670bbc29f7728"
+    )
+  ensure
+    frame_source&.stop
+  end
+
+  def png_scanlines(png)
+    offset = Vizcore::Renderer::PngWriter::SIGNATURE.bytesize
+    idat = +"".b
+
+    while offset < png.bytesize
+      length = png.byteslice(offset, 4).unpack1("N")
+      type = png.byteslice(offset + 4, 4)
+      data = png.byteslice(offset + 8, length)
+      idat << data if type == "IDAT"
+      offset += 12 + length
+    end
+
+    Zlib::Inflate.inflate(idat)
   end
 end
